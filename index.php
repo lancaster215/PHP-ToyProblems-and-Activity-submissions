c0d387752ad8114bdacaf66880c796c8275d3f1a
lancaster215 <francisco.ifurung@bicol-u.edu.ph>
2020-03-27 15:10:58 +0800

    done define and spaceship

--- index.php
+++ index.php
@@ -8,12 +8,16 @@ interface IProject
     public function bultInVersion();
     public function technologyStack();
     public function developers();
+    public const projectName = "Boom.Camp Portal V1.0.0";
 }
 
 class Details implements IProject
 {
-    // public $projectName = "Boom.Camp Portal V1.0.0";
-    public function iClient($name="Glenn Perez") :? String{
+    public function projectName() :? String{
+        return IProject::projectName;
+    }
+    public function iClient(String $name="Glenn Perez") :? String{
+        echo "<br>";
         return $name;
     }
     public function myProjectManager($manager="Scrum Master") :? String{
@@ -40,10 +44,41 @@ class Details implements IProject
 
 $details = new Details();
 
+echo $details->projectName();
 echo $details->iClient();
 echo $details->myProjectManager();
 echo $details->yearDeployed();
 echo $details->bultInVersion();
 var_dump($details->technologyStack());
 var_dump($details->developers());
+?>
+
+
+<?php
+define('ENUM_RATINGS', array(
+    "Needs more improvement",
+    "Failed in Boom.Camp Training",
+    "Pass in Boom.Camp and can proceed in Graduation",
+));
+
+function compareIfPass($a, $b){
+    $compare = $a <=> $b;
+    switch($compare){
+        case 0:
+            echo ENUM_RATINGS[0]."<br>";
+            break;
+        case -1:
+            echo ENUM_RATINGS[1]."<br>";
+            break;
+        case 1:
+            echo ENUM_RATINGS[2]."<br>";
+            break;
+        default:
+            "Work hard 'till you accomplish anything";
+            break;
+    }
+}
+compareIfPass(90,80);
+compareIfPass(80,90);
+compareIfPass(90,90);
 ?>
